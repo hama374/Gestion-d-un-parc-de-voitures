2116d63813fc3c72fd67188d9d035d322aefd7f5
hama374 <djaoudhamama15@gmail.com>
2026-03-08 22:06:32 -0400

    Créer la classe Parc et ses attributs

--- ATELIER1.py
+++ ATELIER1.py
@@ -9,3 +9,10 @@ class Voiture:
             print("Matricule :", self.matricule)
             print("Marque :", self.marque)
             print("Couleur :", self.couleur)
+class Parc:
+
+    def __init__(self, id, adresse, capacite):
+         self.id = id
+         self.adresse = adresse
+         self.capacite = capacite
+         self.listeVoitures = []
